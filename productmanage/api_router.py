710884a571f3ad393c13b492993e75eb51178fb8
dream <seniordev715@gmail.com>
2023-06-09 21:06:57 +0900

    add productphotoviewset

--- productmanage/api_router.py
+++ productmanage/api_router.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import SimpleRouter, DefaultRouter
 
 from users.api import UserViewSet
-from product.api import ProductViewSet
+from product.api import ProductViewSet, ProductPhotoViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
 router.register('product', ProductViewSet)
+router.register('productphoto', ProductPhotoViewSet)
 
 app_name = 'api'
 urlpatterns = router.urls
