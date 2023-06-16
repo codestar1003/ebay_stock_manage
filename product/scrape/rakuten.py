61a1d6e347c0790835b580417f81cb0e4fc03d8e
dream <seniordev715@gmail.com>
2023-06-16 11:34:54 +0900

    fix rakuten scraping engine

--- product/scrape/rakuten.py
+++ product/scrape/rakuten.py
@@ -9,14 +9,21 @@ class ScrapingEngine:
     def scrape_data(self, source_url):
         options = webdriver.ChromeOptions() 
         options.headless = True
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options=options)
         driver.get(source_url)
+        loaded = None
         data = {}
-        time.sleep(10)
-        dom = bs(driver.page_source, "html.parser")
+        while loaded is None:
+            dom = bs(driver.page_source, "html.parser")
+            loaded = dom.find('div', attrs={'id': 'priceCalculationConfig'})['data-price']
+            time.sleep(5)
 
         data['title_jp'] = convert_text(dom.find('span', attrs={'class': 'normal_reserve_item_name'}).b.text)
         data['price'] = int(dom.find('div', attrs={'id': 'priceCalculationConfig'})['data-price'])
+        if dom.find('ul', attrs={'class': 'point-summary__campaign___2KiT- point-summary__multiplier-up___3664l point-up'}):
+            point_info = dom.find('ul', attrs={'class': 'point-summary__campaign___2KiT- point-summary__multiplier-up___3664l point-up'}).find_all('li')
+            multi = int(point_info[-1].text.strip('倍UP'))
+            data['point'] = int(data['price']*multi/100)
         data['description_jp'] = []
         descriptions = dom.find('span', attrs={'class': 'item_desc'})
         if descriptions:
