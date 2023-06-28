0be5665f0111820707c7bc6138373af16d60bac0
dream <seniordev715@gmail.com>
2023-06-28 23:48:24 +0900

    add ebay credential in user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 from dry_rest_permissions.generics import authenticated_users
 
@@ -8,6 +9,30 @@ class User(AbstractUser):
     email = models.EmailField(max_length=250)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    app_id = models.CharField(
+        _('app Id'),
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    dev_id = models.CharField(
+        _('dev Id'),
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    cert_id = models.CharField(
+        _('cert Id'),
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    ebay_token = models.CharField(
+        _('ebay Token'),
+        max_length=255,
+        null=True,
+        blank=True
+    )
 
     
     def has_read_permission(request):
